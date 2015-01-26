3cc2a0b6cbf32ad60be67795b89e0da429b7cdbb
Russell Tan <sell_nat@yahoo.com>
2015-01-26 14:53:47 -0800

    Starting ALU lab

---
+++ ALUWithControl/ALUWithControl.v
@@ -0,0 +1,43 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    14:44:04 01/26/2015 
+// Design Name: 
+// Module Name:    ALUWithControl 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module ALUWithControl(
+    input [31:0] a,
+    input [31:0] b,
+    input [3:0] ALUctl,
+    output reg [31:0] ALUOut,
+    output reg [1:0] zero
+    );
+	 
+	 always @(a,b,ALUctl)
+	 begin
+		case(ALUctl)
+			4'b0000: ALUOut = a&b;
+			4'b0001: ALUOut = a|b;
+			4'b0010: ALUOut = a+b;
+			4'b0110: ALUOut = a-b;
+			4'b0111: ALUOut = a<b;
+		endcase
+		if (ALUOut == 0) zero = 1;
+		else zero = 0;
+	end
+
+
+endmodule
---
+++ ALUWithControl/fuseRelaunch.cmd
@@ -0,0 +1,1 @@
+-intstyle "ise" -incremental -lib "unisims_ver" -lib "unimacro_ver" -lib "xilinxcorelib_ver" -o "C:/Users/Russell/git/Architecture/ALUWithControl/ALUWithControl_isim_beh.exe" -prj "C:/Users/Russell/git/Architecture/ALUWithControl/ALUWithControl_beh.prj" "work.ALUWithControl" "work.glbl" 
