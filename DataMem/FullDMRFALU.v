4da150bf5d09d768a9cf0db8288c33c049569292
Russell Tan <sell_nat@yahoo.com>
2015-02-25 08:32:56 -0800

    Making the schematic look right

--- DataMem/FullDMRFALU.v
+++ DataMem/FullDMRFALU.v
@@ -18,29 +18,32 @@
 // Additional Comments: 
 //
 //////////////////////////////////////////////////////////////////////////////////
-module FullDMRFALU( rs,rt,rd,SEin,FuncCode,Regsel,ALUsel,ALUOp,MemWrite,MemRead,MemToRegSel,RegWrite,clk,Zero);
+module FullDMRFALU( rs,rt,rd,SEin,FuncCode,Regsel,ALUsel,ALUOp,MemWrite,MemRead,MemToRegSel,RegWrite,clk,Zero,ALUOut);
 
-	 input Regsel,ALUsel,ALUOp,MemWrite,MemRead,MemToRegSel,RegWrite,clk;
+	 input Regsel,ALUsel,MemRead,MemToRegSel,RegWrite,clk;
+	 input [1:0] MemWrite;
+	 input [1:0] ALUOp;
 	 input [5:0] FuncCode;
 	 input [4:0] rs,rt,rd;
 	 input [15:0] SEin;
 	 
 	 output Zero;
+	 output wire [31:0] ALUOut;
 	 
 	 wire [3:0] ALUCtl;
 	 wire [4:0] WriteReg;
-	 wire [31:0] RegToMux2, A, B, SEOut, ALUOut, ReadDataOut, ReadData;
+	 wire [31:0] RegToMux2, A, B, SEOut, ReadDataOut, ReadData;
 	 
 
-	DataMem DataMem(.clk(clk),.Address(ALUResultWire),.WriteData(ReadData2Wire),.MemWrite(MemWrite),.MemRead(MemRead),.ReadData(ReadDataOut));
+	DataMem DataMem(.clk(clk),.Address(ALUOut),.WriteData(RegToMux2),.MemWrite(MemWrite),.MemRead(MemRead),.ReadData(ReadDataOut));
 
 	SignExtender SignExtender(.SEin(SEin),.SEout(SEOut));
 	
 	TwoOneMux32 TwoOneMux32(.A(RegToMux2),.B(SEOut),.sel(ALUsel),.out(B));
 	
-	TwoOneMux321 TwoOneMux321(.A(ALUOut),.B(ReadDataOut),.sel(MemToReg),.out(ReadData));
+	TwoOneMux321 TwoOneMux321(.A(ALUOut),.B(ReadDataOut),.sel(MemToRegSel),.out(ReadData));
 	
-	TwoOneMux5 TwoOneMux5(.A(rt),.B(rd),.sel(RegSel),.out(WriteReg));
+	TwoOneMux5 TwoOneMux5(.A(rt),.B(rd),.sel(Regsel),.out(WriteReg));
 	
 	ALUControl ALUControl(.ALUOp(ALUOp),.FuncCode(FuncCode),.ALUctl(ALUCtl));
 	
---
+++ DataMem/FullRFDMALUTest.v
@@ -0,0 +1,194 @@
+`timescale 1ns / 1ps
+
+////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer:
+//
+// Create Date:   07:49:45 02/25/2015
+// Design Name:   FullDMRFALU
+// Module Name:   C:/Users/Russell/git/Architecture/DataMem/FullRFDMALUTest.v
+// Project Name:  DataMem
+// Target Device:  
+// Tool versions:  
+// Description: 
+//
+// Verilog Test Fixture created by ISE for module: FullDMRFALU
+//
+// Dependencies:
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+////////////////////////////////////////////////////////////////////////////////
+
+module FullRFDMALUTest;
+
+	// Inputs
+	reg [4:0] rs;
+	reg [4:0] rt;
+	reg [4:0] rd;
+	reg [15:0] SEin;
+	reg [5:0] FuncCode;
+	reg Regsel;
+	reg ALUsel;
+	reg [1:0]ALUOp;
+	reg MemWrite;
+	reg MemRead;
+	reg MemToRegSel;
+	reg RegWrite;
+	reg clk;
+
+	// Outputs
+	wire Zero;
+	wire ALUOut;
+
+	// Instantiate the Unit Under Test (UUT)
+	FullDMRFALU uut (
+		.rs(rs), 
+		.rt(rt), 
+		.rd(rd), 
+		.SEin(SEin), 
+		.FuncCode(FuncCode), 
+		.Regsel(Regsel), 
+		.ALUsel(ALUsel), 
+		.ALUOp(ALUOp), 
+		.MemWrite(MemWrite), 
+		.MemRead(MemRead), 
+		.MemToRegSel(MemToRegSel), 
+		.RegWrite(RegWrite), 
+		.clk(clk), 
+		.Zero(Zero),
+		.ALUOut(ALUOut)
+	);
+	always
+	begin
+		#50 clk = 0;
+		#50 clk = 1;
+	end
+
+	initial begin
+		// Initialize Inputs
+		rs = 0;
+		rt = 0;
+		rd = 0;
+		SEin = 0;
+		FuncCode = 0;
+		Regsel = 1;
+		ALUsel = 1;
+		ALUOp = 0;
+		MemWrite = 1;
+		MemRead = 1;
+		MemToRegSel = 1;
+		RegWrite = 1;
+		clk = 0;
+
+		// Wait 100 ns for global reset to finish
+		#50;
+        
+		// Add stimulus here
+		rs = 0;
+		rt = 0;
+		rd = 0;
+		SEin = 16'h0014;
+		FuncCode = 0;
+		Regsel = 1;
+		ALUsel = 1;
+		ALUOp = 0;
+		MemWrite = 1;
+		MemRead = 1;
+		MemToRegSel = 1;
+		RegWrite = 1;
+		
+		#100;
+		rs = 0;
+		rt = 0;
+		rd = 0;
+		SEin = 16'h0028;
+		FuncCode = 0;
+		Regsel = 1;
+		ALUsel = 1;
+		ALUOp = 0;
+		MemWrite = 1;
+		MemRead = 1;
+		MemToRegSel = 1;
+		RegWrite = 1;
+///////////////////////////////////////////////////
+		#100;
+		rs = 0;
+		rt = 0;
+		rd = 0;
+		SEin = 0;
+		FuncCode = 4'b00000;
+		Regsel =1;
+		ALUsel = 1;
+		ALUOp = 2'b10;
+		MemWrite = 1;
+		MemRead =1;
+		MemToRegSel = 1;
+		RegWrite = 1;
+			
+
+		#100;
+		rs = 0;
+		rt = 0;
+		rd = 0;
+		SEin = 0;
+		FuncCode = 4'b0010;
+		Regsel = 1;
+		ALUsel = 1;
+		ALUOp = 2'b10;
+		MemWrite = 1;
+		MemRead = 1;
+		MemToRegSel = 1;
+		RegWrite = 1;
+			
+
+		#100;
+		rs = 0;
+		rt = 0;
+		rd = 0;
+		SEin = 0;
+		FuncCode = 4'b0100;
+		Regsel = 1;
+		ALUsel = 1;
+		ALUOp = 2'b10;
+		MemWrite = 1;
+		MemRead = 1;
+		MemToRegSel = 1;
+		RegWrite = 1;
+	
+
+		#100;
+		rs = 0;
+		rt = 0;
+		rd = 0;
+		SEin = 0;
+		FuncCode = 4'b0101;
+		Regsel = 1;
+		ALUsel = 1;
+		ALUOp = 2'b10;
+		MemWrite = 1;
+		MemRead = 1;
+		MemToRegSel = 1;
+		RegWrite = 1;
+		
+
+		#100;
+		rs = 0;
+		rt = 0;
+		rd = 0;
+		SEin = 0;
+		FuncCode = 4'b1010;
+		Regsel = 1;
+		ALUsel = 1;
+		ALUOp = 2'b10;
+		MemWrite = 1;
+		MemRead = 1;
+		MemToRegSel = 1;
+		RegWrite = 1;
+			
+	end
+      
+endmodule
+
